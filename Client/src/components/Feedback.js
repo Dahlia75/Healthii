e81793dfeb4019c13f5ce961b10505e61e6eba17
mehdibz <bz.mehdi@gmail.com>
2018-06-12 08:01:44 +0000

    feedback updated

--- Client/src/components/Feedback.js
+++ Client/src/components/Feedback.js
@@ -1,57 +1,14 @@
 import React from 'react';
 import ReactTable from "react-table";
 import 'react-table/react-table.css'
-
-// import FeedbackForm from './FeedbackForm';
-import PopupFeedback from './PopupFeedback';
+import PopupReport from './PopupReport';
 import Api from '../lib/api.js';
+import '../App.css';
+import Logo from '../img/HC2Go1.png';
+//import "../css/feedback.css";
 // import Star from './Star';
 
-import '../App.css';
-//import "../css/feedback.css";
-import Logo from '../img/HC2Go1.png';
-
-
-
-const range = len => {
-  const arr = [];
-  for (let i = 0; i < len; i++) {
-    arr.push(i);
-  }
-  return arr;
-};
-
-const newPerson = () => {
-  // const statusChance = Math.random();
-  return {
-    service_name: 'physiotherapy',
-    name: 'Dude Awesomepants',
-    address: '123 gastown, vancouver',
-    m_history: "Oh I am AMAZING!!!!!",
-    gender: 'female',
-    age: '30',
-    date: 'monday, july 15, 2018',
-    time: {
-      from : 2452845824,
-      to: 4248248,
-    },
-    status: 'pending',
-    button: <PopupFeedback />
-
-    //if pending, want to link to profile, then in profile, will accept or decline
-  };
-};
-
- function makeData(len = 10) {
-  return range(len).map(d => {
-    return {
-      ...newPerson(),
-      children: range(10).map(newPerson)
-    };
-  });
-}
-
- const Tips = () =>
+const Tips = () =>
   <div style={{ textAlign: "center" }}>
     <em>Tip: Hold shift when sorting to multi-sort!</em>
   </div>;
@@ -61,29 +18,44 @@ class Feedback extends React.Component {
   constructor() {
     super();
     this.state = {
-      data: makeData(),
-      showPopup: false,
-      popup: <PopupFeedback />
+      data: [],
+      showPopup: false
+
     };
   }
 
+  makeData(providers) {
+   return providers.forEach(function(element){
+    element['button'] = <PopupReport />
+   });
+  }
+
   load() {
-      const rid = this.props.match.params.rid;
+      // const rid = this.props.match.params.rid;
 
-      Api.post(`/api/reviews/${rid}/feedback`).then(service => {
-          // const providers = service.providers.reduce((acc, provider) => {
-          //     return acc.concat(current.providers);
-          // }, []);
-      // console.log("service: ", service.providers);
+      // Api.post(`/api/reviews/${rid}/feedback`).then(service => {
+      //     // const providers = service.providers.reduce((acc, provider) => {
+      //     //     return acc.concat(current.providers);
+      //     // }, []);
+      // // console.log("service: ", service.providers);
+      // // console.log("##### ",makeData());
+      //     this.setState({
 
-          this.setState({
-              // providers: service.providers,
-              // reviews: service.reviews,
-              // selectedService : service.service_name,
-              // selectedList : service.providers,
-              // selectedSid : sid
-          });
-      });
+      //         // providers: service.providers,
+      //         // reviews: service.reviews,
+      //         // selectedService : service.service_name,
+      //         // selectedList : service.providers,
+      //         // selectedSid : sid
+      //     });
+      // });
+      Api.get(`/api/reviews`).then(providers => {
+        // providers.push('button: <PopupReport/>');
+        this.makeData(providers);
+        
+         this.setState({
+                       data: providers
+                      });
+      })
   }
 
   componentDidMount() {
@@ -113,28 +85,16 @@ class Feedback extends React.Component {
                     accessor: "name"
                   },
                   {
-                    Header: "Age",
-                    accessor: "age"
-                  },
-                  {
                     Header: "Gender",
                     accessor: "gender"
                   },
-
-                ]
-              },
-              {
-                Header: "Appointment Info",
-                columns: [
-
                   {
-                    Header: "Address",
-                    accessor: "address"
+                    Header: "Service",
+                    accessor: "service_name"
                   },
-
                   {
-                    Header: "Status",
-                    accessor: "status"
+                    Header: "Date",
+                    accessor: "date"
                   }
                 ]
               },
