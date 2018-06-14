8ac261d6bacc557ba3126b63c46be72556201bcf
Dahlia75 <d.aljabari75@gmail.com>
2018-06-13 18:32:45 -0700

    fix-profile-css

--- Client/src/components/ProviderCard.js
+++ Client/src/components/ProviderCard.js
@@ -26,35 +26,31 @@ export default class ProviderCard extends Component {
     const sid = this.props.serviceId;
     const path = `/services/${sid}/providers/${pid}`;
     console.log("path for provider ", path);
-    const genderSign = gender === 'female' ? 'fa fa-venus' : 'fa fa-mars';
     const pimg = `http://localhost:3001/img/providers/${pid}.jpg`;
     console.log("pimg", pimg);
     const card =
 
     <div className="card text-center">
-    <nav className="provider-container">
-                <div className='name-provider'><h4 className="card-title">{ name }</h4></div>
-                {/* <div className='age-provider'><p > { age } years old</p></div> */}
-                <i className= { genderSign }> </i>
-                
-            </nav>
-        <img className="card-img-top img-fluid rounded-circle hvr-grow" src= { pimg } alt="Person" />
-        <div className="card-block">
+      <nav className="provider-container">
+        <div className='name-provider'><h4 className="card-title">{ name }</h4></div>
+        {/* <div className='age-provider'><p > { age } years old</p></div> */}
+      </nav>
+      <img className="card-img-top img-fluid rounded-circle hvr-grow" src= { pimg } alt="Person" />
+      <div className="card-block">
+        <div className="card-text">
+            <Link to={path} className="button-provider-profile"> Profile
+            </Link>
+            <div className="cta-provider">
+                <button className="button-provider-book" onClick = { this.onBook.bind(this) }>
+                  Book now
+                </button>
+            </div>
+        </div>
         <div id="test">
 
-
-            <div className="card-text">
-                <Link to={path} className="button-provider-profile"> Profile
-                </Link>
-                <div className="cta-provider">
-                    <button className="button-provider-book" onClick = { this.onBook.bind(this) }>
-                      <span className="">Book now</span>
-                    </button>
-                </div>
-            </div>
             <i className = 'fa fa-remove fa-2x float-right hvr-grow' onClick = {this.onRemoveProvider.bind(this)}> </i>
         </div>
-        </div>
+      </div>
     </div>;
     return card;
 
